7a0e9ec2775a77426df07b58bb7af681e08f05c0
Henrik Sorg <henrik@reif-sorg.de>
2023-12-02 11:21:15 +0100

    first style contact

---
+++ js/contact.js
@@ -0,0 +1,44 @@
+let alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
+
+function renderContacts(){
+    insertDirectory();
+    insertContacts();
+}
+
+function insertDirectory(){
+    for (let i = 0; i < alphabet.length; i++) {
+        const letter = alphabet[i];
+        document.getElementById('contacts-overview').innerHTML += insertDirectoryTemplate(letter);
+    }
+}
+
+function insertDirectoryTemplate(letter){
+return /*html*/`
+    <div class="contact-section" id="section-${letter.toLowerCase()}">
+        <span class="contact-section-letter">${letter}</span>
+    </div>
+`
+}
+
+function insertContacts(){
+    for (let i = 0; i < alphabet.length; i++) {
+        let letter = alphabet[i].toLowerCase();
+        document.getElementById('section-'+letter).innerHTML += insertContactsTemmplate();
+        // console.log('section-'+letter)
+    }
+    
+}
+
+function insertContactsTemmplate(){
+return /*html*/`
+    <div class="contact">
+        <div class="initial">JD</div>
+        <div style="display: flex;flex-direction: column;">
+            <span class="name">John Doe</span>
+            <span class="email">email@email1234.com</span>
+        </div>
+    </div>
+`
+}
+
+renderContacts();
