343f4a9b33db1fb34cd146a729dace8b469067f1
Ibrahima SOURABIE <ge65yub@mytum.de>
2023-12-05 16:28:33 +0100

    login function added

--- js/login_out.js
+++ js/login_out.js
@@ -2,34 +2,55 @@
 
 let users = [
   {
-    firstName: "Join",
-    lastName: "803",
-    email: "join803@gmail.com",
+    name: "Ibrahima",
+    email: "join803@ibrahima.de",
     password: "1234",
   },
   {
-    firstName: "Is",
-    lastName: "803",
-    email: "join802@gmail.com",
-    password: "1284",
+    name: "Pascal",
+    email: "join803@pascal.de",
+    password: "1235",
+  },
+  {
+    name: "Henrik",
+    email: "join803@henrik.de",
+    password: "1236",
+  },
+  {
+    name: "Thomas",
+    email: "join803@thomas.com",
+    password: "1237",
   },
 ];
 
-const checkUserData = function () {
-  let email = document.getElementById("email");
-  let password = document.getElementById("password");
-  for (let i = 0; i < users.length; i++) {
-    const user = users[i];
-    if (
-      email.value.toLowerCase() === user["email"].toLowerCase() &&
-      password.value === user["password"]
-    ) {
-      logUserIn();
-    }
+///////// SINGN UP    /////////////////////////////////////////////////
+
+const registerUser = function (name, email, password) {
+  let newUser = new Object();
+  newUser.name = name;
+  newUser.email = email;
+  newUser.password = password;
+  users.push(newUser);
+  localStorage.setItem("users", JSON.stringify(users));
+};
+
+const loadUsers = function () {
+  let existUsers = localStorage.getItem("users");
+  if (existUsers) {
+    users = JSON.parse(existUsers);
   }
 };
 
-const logUserIn = function () {};
+/////////////// LOG in /////////////////////////////////////////////////
+
+const checkUserData = function () {
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    const email = user["email"];
+    const password = user["password"];
+    checkDataForValidation(email, password);
+  }
+};
 
 const openHelp = function () {
   let summPage = document.getElementById("summary-page");
@@ -48,19 +69,79 @@ const showPrivacyPolicy = function () {
   let privacyPage = document.getElementById("privacy-page");
   let userMenu = document.getElementById("submenu-user");
   let helpPage = document.getElementById("help-page");
-  helpIcon.classList.add("d-none");
-  loginBtn.classList.add("d-none");
-  summPage.classList.add("d-none");
-  userMenu.classList.add("d-none");
+  hideIfPrivayPage(helpIcon, loginBtn, summPage, userMenu, helpPage);
   helpPage.style.display = "";
-  helpPage.classList.add("d-none");
   privacyPage.classList.remove("d-none");
   privacyPage.style.display = "inline-block";
 };
 
-// const closeHelp = function () {
-//   let returnBtn = document.getElementById("return-btn");
-//   let help = document.getElementById("help-icon");
+const hideIfPrivayPage = function (
+  helpIcon,
+  loginBtn,
+  summPage,
+  userMenu,
+  helpPage
+) {
+  helpIcon.classList.add("d-none");
+  loginBtn.classList.add("d-none");
+  summPage.classList.add("d-none");
+  userMenu.classList.add("d-none");
+  helpPage.classList.add("d-none");
+};
 
-//   help.style.display = "";
-// };
+function isPasswordValid(givenPassword, currentPassword) {
+  if (givenPassword == currentPassword) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEmailValid(givenEmail, currentEmail) {
+  if (givenEmail.toLowerCase() == currentEmail.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+const generateError = (errorType, errorMsg) => {
+  const emailError = document.getElementById("emailError");
+  const passwordError = document.getElementById("passwordError");
+  let emailField = document.getElementById("email");
+  let passwordField = document.getElementById("password");
+  if (errorType == "email") {
+    emailField.style.border = "2px solid red";
+    emailError.innerHTML = errorMsg;
+  } else if (errorType == "password") {
+    passwordField.style.border = "2px solid red";
+    passwordError.innerHTML = errorMsg;
+  }
+};
+
+const openSummaryPage = function () {
+  window.location.href = "../html/summary.html";
+};
+
+const checkDataForValidation = function (currentEmail, currentPassword) {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  if (isEmailValid(email.value, currentEmail)) {
+    checkPassword(password, currentPassword);
+  } else {
+    generateError("email", "Please Enter a valid email");
+  }
+};
+
+const checkPassword = function (password, currentPassword) {
+  if (isPasswordValid(password.value, currentPassword)) {
+    openSummaryPage();
+  } else {
+    generateError("password", "Please Enter a valid password");
+  }
+};
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    checkDataForValidation();
+  }
+});
