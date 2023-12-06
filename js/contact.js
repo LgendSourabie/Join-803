6a2419aee4e727c0546a36e2f878c59ee66ac989
Henrik Sorg <henrik@reif-sorg.de>
2023-12-06 19:58:13 +0100

    edit contact

--- js/contact.js
+++ js/contact.js
@@ -1,37 +1,37 @@
-let alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
+let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 let contacts = [
     {
-        "name" : "Ibrahima Sourabie",
-        "telephone" : "0123456789",
-        "email" : "ibrahima@join803.de"
+        "name": "Ibrahima Sourabie",
+        "telephone": "0123456789",
+        "email": "ibrahima@join803.de"
     },
     {
-        "name" : "Pascal Wagner",
-        "telephone" : "0123456789",
-        "email" : "pascal@join803.de"
+        "name": "Pascal Wagner",
+        "telephone": "0123456789",
+        "email": "pascal@join803.de"
     },
     {
-        "name" : "Thomas Jilge",
-        "telephone" : "0123456789",
-        "email" : "thomas@join803.de"
+        "name": "Thomas Jilge",
+        "telephone": "0123456789",
+        "email": "thomas@join803.de"
     },
     {
-        "name" : "Henrik Sorg",
-        "telephone" : "0123456789",
-        "email" : "henrik@join803.de"
+        "name": "Henrik Sorg",
+        "telephone": "0123456789",
+        "email": "henrik@join803.de"
     }
 ];
 
 let highlightedContact;
 
 
-function renderContactSection(){
+function renderContactSection() {
     document.getElementById('render-container').innerHTML = renderContactSectionTemplate();
     renderContacts();
 }
 
 
-function renderContactSectionTemplate(){
+function renderContactSectionTemplate() {
     return /*html*/`
         <div class="scroll-container">
                 <div class="contacts-overview-container">
@@ -56,30 +56,30 @@ function renderContactSectionTemplate(){
 }
 
 
-function renderContacts(){
+function renderContacts() {
     insertDirectory();
     insertContacts();
     // showContact();
 }
 
-function openContact(i){
+function openContact(i) {
     let selectedContact = 'contact-' + i;
-    
-    if(selectedContact == highlightedContact){
-        hideContact();  
+
+    if (selectedContact == highlightedContact) {
+        hideContact();
         removeHighligtContact(i);
-    }else{
+    } else {
         removeHighligtContact(i);
         showContact(i);
         highligtContact(i);
     }
 }
 
-function hideContact(){
+function hideContact() {
     document.getElementById('show-contact').innerHTML = '';
 }
 
-function showContact(i){
+function showContact(i) {
     let name = contacts[i]['name'];
     let email = contacts[i]['email'];
     let telephone = contacts[i]['telephone'];
@@ -87,13 +87,13 @@ function showContact(i){
 }
 
 
-function showContactTemplate(name, email, telephone, i){
+function showContactTemplate(name, email, telephone, i) {
     return /*html*/`
         <div>
             <div class="initial-name-container">
                 <div class="initial-single-view">JD</div>
                 <div class="name-edit-delete">
-                    <div class="name-single-view">${name}</div>
+                    <div id="name-single-view">${name}</div>
                     <div class="edit-and-delete">
                         <div onclick="editContact(${i})"><img src="../icons/editContact.svg">Edit</div>
                         <div onclick="deleteContact(${i})"><img src="../icons/delete.svg">Delete</div>
@@ -103,19 +103,19 @@ function showContactTemplate(name, email, telephone, i){
             </div>
             <div class="contact-information-headline">Contact Information</div>
             <div class="contact-information">
-            <div class="email-single-view">
+            <div class="email-single-view-container">
                 <span>
                     Email
                 </span>
-                <span>
+                <span id="email-single-view">
                     ${email}
                 </span>
             </div>
-            <div class="phone-single-view">
+            <div class="phone-single-view-container">
                 <span>
                     Phone
                 </span>
-                <span>
+                <span id="phone-single-view">
                     ${telephone}
                 </span>
             </div>
@@ -123,22 +123,22 @@ function showContactTemplate(name, email, telephone, i){
     `
 }
 
-function insertDirectory(){
+function insertDirectory() {
     for (let i = 0; i < alphabet.length; i++) {
         const letter = alphabet[i];
         document.getElementById('contacts-overview').innerHTML += insertDirectoryTemplate(letter);
     }
 }
 
-function insertDirectoryTemplate(letter){
-return /*html*/`
+function insertDirectoryTemplate(letter) {
+    return /*html*/`
     <div class="contact-section d-none" id="section-${letter.toLowerCase()}">
         <span class="contact-section-letter">${letter}</span>
     </div>
 `
 }
 
-function insertContacts(){
+function insertContacts() {
     for (let i = 0; i < contacts.length; i++) {
         let name = contacts[i]['name']
         let initialLetter = name.charAt(0).toLowerCase();
@@ -150,7 +150,7 @@ function insertContacts(){
     }
 }
 
-function insertContactsTemmplate(i, name){
+function insertContactsTemmplate(i, name) {
     let email = contacts[i]['email']
     return /*html*/`
     <div class="contact" id="contact-${i}" onclick="openContact(${i})">
@@ -165,44 +165,57 @@ function insertContactsTemmplate(i, name){
 `
 }
 
-function highligtContact(i){
+function highligtContact(i) {
     id = 'contact-' + i
     highlightedContact = id
     document.getElementById(id).classList.add('highlight-contact')
 }
 
-function removeHighligtContact(){
+function removeHighligtContact() {
     id = highlightedContact;
-    if (highlightedContact){
+    if (highlightedContact) {
         document.getElementById(id).classList.remove('highlight-contact');
         highlightedContact = ''
     }
-    
+
 }
 
 
-function createContact(){
+function createContact() {
     let name = document.getElementById('name-input').value
     let telephone = document.getElementById('telephone-input').value
     let email = document.getElementById('email-input').value
     console.log(contacts)
-    contacts.push({'name' : name, 'telephone': telephone, 'email': email}); 
-    console.log(contacts)
+    contacts.push({ 'name': name, 'telephone': telephone, 'email': email });
+    console.log(contacts);
+    closeAddContact();
+    // document.getElementById('main-container').innerHTML = ''
     renderContactSection();
 }
 
-function openAddContact(){
+function openAddContact() {
     document.getElementById('add-contact-bg').classList.remove('d-none')
 }
 
-function closeAddContact(){
+function closeAddContact() {
     document.getElementById('add-contact-bg').classList.add('d-none')
 }
 
-function editContact(){}
+function editContact(i) {
+    openAddContact();
+    let name = document.getElementById('name-input').value
+    let telephone = document.getElementById('telephone-input').value
+    let email = document.getElementById('email-input').value
+    getName = document.getElementById('name-single-view').innerHTML
+    getEmail = document.getElementById('email-single-view').innerHTML
+    getPhone = document.getElementById('phone-single-view').innerHTML
+    name = getName;
+    email = getEmail;
+    telephone = getPhone;
+}
 
-function deleteContact(i){
-    contacts.splice(i,1)
+function deleteContact(i) {
+    contacts.splice(i, 1)
     removeHighligtContact();
     renderContactSection();
 }
