265649c46a84529a43c334d28d0b08f057355793
ThomasJilge <thomasjilge@gmx.de>
2023-12-07 22:16:25 +0100

    function createTask added (not ready)

--- js/addTask.js
+++ js/addTask.js
@@ -1,3 +1,24 @@
+let contacts = [
+    {
+        name: "Pascal Wagner"
+    },
+    {
+        name: "Henrik Sorg"
+    },
+    {
+        name: "Ibrahima Sourabie"
+    },
+    {
+        name: "Thomas Jilge"
+    },
+];
+
+let tasks = [];
+let users = [];
+let subtasks = 0;
+let usersSelect = [];
+let usersAssigned = [];
+
 function changeColorPrio(element) {
     if (element.classList.contains('prioUrgent')) {
         element.style.backgroundColor = 'rgb(255,61,0)';
@@ -8,4 +29,49 @@ function changeColorPrio(element) {
     }
     element.style.color = 'white';
 }
+
+
+function addContacts() {
+    // let getContacts = document.getElementById('assigned');
+    // let newElement = document.createElement('new');
+    let select = document.getElementById('select');
+    select.innerHTML = '';
+
+    select.innerHTML = `
+        <option id="assigned" value="">Selected contacts to assign</option>
+    `;
+
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        select.innerHTML += `
+        <div class="assignedAddTask addTaskOverview">
+        <span>Assigned to</span>
+            <select id="select">
+                <option value="">Pascal Wagner</option>
+                <option value="">Henrik Sorg</option> 
+                <option value="">Ibrahima Sourabie</option>
+                <option value="">Thomas Jilge</option>
+            </select>
+         </div>
+        `;
+}
+}
+
+
+function createTask() {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let date = document.getElementById('date').value;
+    let category = document.getElementById('category').value;
+    let assignedTo = document.getElementById('select').value;
+
+    tasks.push({
+        title: title,
+        description: description, 
+        date: date,
+        category: category,
+        assignedTo: assignedTo
+    })
+}
+
 
