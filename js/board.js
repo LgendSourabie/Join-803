b4eb2a0c843436a35c9f563402cce123601cff5b
PasTof <125820486+PasTof@users.noreply.github.com>
2024-01-07 15:09:58 +0100

    edit task

--- js/board.js
+++ js/board.js
@@ -388,6 +388,97 @@ function edittask(i) {
 
 function edittasktemplate(i){
   return /*html*/`
-    
+            <form class="addTaskOverviewContainer" onsubmit="createTask(); return false">
+        <div class="addTaskContainerLeftRight">
+            <div class="addTaskContainerOneflyin">
+                <div class="test1">
+                <div class="groupButtonForm">
+                    <div class="test2">
+                    <div class="h2Container">
+                        <h2>Add Task</h2>
+                    </div>
+                </div>
+                <div class="test3">
+                    <div class="containerLeft">
+                        <div class="titleAddTask addTaskOverview">
+                            <span class="containerLeftSpan">Title
+                                <span class="star">*</span>
+                            </span>
+                            <input onclick="changeBorderColor(this)" id="title" class="inputAddTask" type="text" placeholder="Enter a title" required>
+                        </div>
+                        <div class="descriptionAddTask addTaskOverview">
+                            <span class="containerLeftSpan">Description</span>
+                            <textarea onclick="changeBorderColor(this)" id="description" class="textAreaAddTask" placeholder="Enter a Description"></textarea>
+                        </div>
+                        <div class="assignedAddTask addTaskOverview">
+                            <span class="containerLeftSpan">Assigned to</span>
+                            <div id="dropdown" class="dropdown" onclick="handleDropdownClick(this)">Select contacts to assign</div>
+                              <div class="test6" onclick="handleDropdownClick(this)">
+                                <img class="dropDownImg" src="../img/img/arrow_drop_down.svg" alt="">
+                              </div>
+                            <div id="options" class="options d-none"></div>
+                              <div id="btn-grp" class="btn"></div>
+                        </div>
+                    </div>
+                    <div class="seperatorContainer"></div>
+                    <div class="containerRight">
+                        <div class="dateAddTask addTaskOverview">
+                            <span class="containerLeftSpan">Due date
+                                <span class="star">*</span>
+                            </span>    
+                            <input onclick="changeBorderColor(this)" id="date" class="inputAddTask" type="date">
+                        </div>
+                        <div class="addTaskOverview">
+                            <span class="containerLeftSpan">Prio</span>
+                            <div id="prio" class="prioSelection">
+                                <img id="colorUrgentImg" onclick="prio('Urgent'); changeColorPrio('colorUrgentImg','colorLowImg', 'colorMediumImg','../img/img/urgent.svg', '../img/img/urgent-white.svg','../img/img/low.svg','../img/img/medium.svg')" class="prio prioUrgentIMG testPrio" src="../img/img/urgent.svg" alt="">
+                                <img id="colorMediumImg" onclick="prio('Medium'); changeColorPrio('colorMediumImg','colorUrgentImg','colorLowImg', '../img/img/medium-yellow.svg', '../img/img/medium.svg','../img/img/urgent.svg','../img/img/low.svg')" class="prio prioMediumIMG testPrio" src="../img/img/medium-yellow.svg" alt="">
+                                <img id="colorLowImg" onclick="prio('Low'); changeColorPrio('colorLowImg','colorMediumImg','colorUrgentImg', '../img/img/low.svg', '../img/img/low-green.svg','../img/img/medium.svg','../img/img/urgent.svg')" class="prio prioLowIMG testPrio" src="../img/img/low.svg" alt="">
+                            </div>
+                        </div>
+                        <div class="categoryAddTask addTaskOverview">
+                            <span class="containerLeftSpan">Category
+                                <span class="star">*</span>
+                            </span>    
+                            <select onclick="changeBorderColor(this)" id="selectCategory">
+                            </select>
+                        </div> 
+                        <div class="subtasksAddTask addTaskOverview">
+                            <span class="spanSubtasks">Subtasks</span>
+                            <div class="test_test">
+                              <input type="text" onclick="changeBorderColor(this)" id="subtasks" class="inputAddTaskSubtask" type="text" placeholder="Add new subtask">
+                              <button type="button" class="buttonSubtask" id="buttonSubtask" >
+                              <img onclick="addNewSubtask(); changeSubtaskImg()" id="subtasksPlusIMG" class="subtasksPlusIMG" src="../img/img/subtasksPlus.svg" alt="">
+                            </button>
+                              <button type="button" class="buttonSubtask" id="buttonSubtask" >
+                              <img onclick="deleteSubtaskInput()" id="subtasksCancelIMG" class="subtasksCancelIMG" src="../img/img/subtasks_cancel.svg" alt=""> 
+                            </button>
+                            </div>                      
+                            <ul id="subtasksList"></ul>
+                        </div> 
+                    </div>
+                </div>
+                </div>
+            </div>
+                <div class="addTaskContainerTwo">
+                        <div class="footerAddTask">
+                            <div class="spanFooter">
+                                <span class="star">*</span>
+                                <span>This field is required</span>
+                            </div>
+                            <div class="footerAddTaskButtons">
+                                <div onclick="closeAddContact()" id="clearButton" class="clearButton">
+                                  <span>Cancel X</span>
+                                </div>
+                                <button id="createTaskButton" class="createTaskButton">
+                                    <span>Create Task</span>
+                                    <img class="imgCheck" src="../img/img/check.svg" alt="">
+                                </button>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </form>
   `
 }
