635e68f8f78b37faa6dbb9aef100ac85b6f4eed3
Ibrahima SOURABIE <ge65yub@mytum.de>
2024-01-23 23:53:33 +0100

    function to dertermine number of tasks optimized

--- js/summary.js
+++ js/summary.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * toggle the menu to allow or disallow the user to logout
+ */
 const showSubmenu = function () {
   let element = document.getElementById('submenu-user');
   element.classList.toggle('d-none');
@@ -9,6 +12,11 @@ let allDate = [];
 let allDateGMT;
 let allDateSTAMP;
 let upComingDate;
+
+/**
+ * determines the due date of the next task from the task array
+ * @returns upcoming due date
+ */
 
 const dueDateNextTask = async function () {
   await loadTasks();
@@ -26,56 +34,64 @@ const dueDateNextTask = async function () {
     upComingDate = allDateGMT[indexDueDate];
   }
 
-  // Parameter wird noch vergeben
-
   let today = new Date(upComingDate);
   let dateArray = today.toDateString().split(' ');
   let day = dateArray[2];
-  let monthNumeric = today.getMonth(); //dateArray[1];
+  let monthNumeric = today.getMonth();
   let year = dateArray[3];
-  let monthArray = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
+  let monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
   return `${monthArray[monthNumeric]} ${day}, ${year}`;
 };
 
+/**
+ * render the number of tasks in every sections (todo, done, urgent, etc.)
+ */
+
 const renderTask = async function () {
+  const [NO_TODO_TASK, NO_DONE_TASK, NO_URGENT_TASK, DUE_DATE_NEXT_TASK, NO_BORD_TASK, NO_PROGRESS_TASK, NO_AWAITING_FEEDBAK_TASK] = await numberTask();
+  let todoID = document.getElementById('todoID');
+  let doneID = document.getElementById('doneID');
+  let urgentID = document.getElementById('urgentID');
+  let due_dateID = document.getElementById('due-dateID');
+  let board_task_number = document.getElementById('board-task-number');
+  let progress_task_number = document.getElementById('progress-task-number');
+  let feedback_task_number = document.getElementById('feedback-task-number');
+  if (todoID) todoID.innerHTML = `${NO_TODO_TASK}`;
+  if (doneID) doneID.innerHTML = `${NO_DONE_TASK}`;
+  if (urgentID) urgentID.innerHTML = `${NO_URGENT_TASK}`;
+  if (due_dateID) due_dateID.innerHTML = `${DUE_DATE_NEXT_TASK}`;
+  if (board_task_number) board_task_number.innerHTML = `${NO_BORD_TASK}`;
+  if (progress_task_number) progress_task_number.innerHTML = `${NO_PROGRESS_TASK}`;
+  if (feedback_task_number) feedback_task_number.innerHTML = `${NO_AWAITING_FEEDBAK_TASK}`;
+};
+
+/**
+ * determine the number of task from the tasks array
+ * @returns no. tasks todo, done, urgent, upcoming date,in board,in progress and awaiting for feedback
+ */
+
+const numberTask = async function () {
   await loadTasks();
-  let taskArray = tasks.map(todo => todo['prio']);
+  let taskArray = tasks.map(todo => todo['taskboard']);
+  let urgentTaskArray = tasks.filter(todo => todo['prio'] === 'urgent');
   let toDo = taskArray.filter(a => a === 'todo');
   let feedback = taskArray.filter(a => a === 'awaitfeedback');
-  let urgent = taskArray.filter(a => a === 'urgent');
-  let board = taskArray.filter(a => a === 'board');
   let progress = taskArray.filter(a => a === 'inprogress');
   let done = taskArray.filter(a => a === 'done');
   const NO_TODO_TASK = toDo.length;
   const NO_DONE_TASK = done.length;
-  const NO_URGENT_TASK = urgent.length;
+  const NO_URGENT_TASK = urgentTaskArray.length;
   const DUE_DATE_NEXT_TASK = await dueDateNextTask();
-  const NO_BORD_TASK = board.length;
+  const NO_BORD_TASK = tasks.length;
   const NO_PROGRESS_TASK = progress.length;
   const NO_AWAITING_FEEDBAK_TASK = feedback.length;
-  document.getElementById('todoID').innerHTML = `${NO_TODO_TASK}`;
-  document.getElementById('doneID').innerHTML = `${NO_DONE_TASK}`;
-  document.getElementById('urgentID').innerHTML = `${NO_URGENT_TASK}`;
-  document.getElementById('due-dateID').innerHTML = `${DUE_DATE_NEXT_TASK}`;
-  document.getElementById('board-task-number').innerHTML = `${NO_BORD_TASK}`;
-  document.getElementById('progress-task-number').innerHTML = `${NO_PROGRESS_TASK}`;
-  document.getElementById('feedback-task-number').innerHTML = `${NO_AWAITING_FEEDBAK_TASK}`;
+  return [NO_TODO_TASK, NO_DONE_TASK, NO_URGENT_TASK, DUE_DATE_NEXT_TASK, NO_BORD_TASK, NO_PROGRESS_TASK, NO_AWAITING_FEEDBAK_TASK];
 };
 
+/**
+ * load task from server to make the tasks available in the dashboard
+ */
 async function loadTasks() {
   tasks = JSON.parse(await getItem('tasks'));
 }
